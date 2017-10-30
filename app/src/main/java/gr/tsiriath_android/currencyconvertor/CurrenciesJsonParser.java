860417320ac0c21a634f49934bcc2f62c15377aa
sakists <sakis_ts@yahoo.com>
2017-10-30 19:04:40 +0200

    2017-10-30-b
    Milestone No 1.
    Next step spinner with icon and text and etitext onafter update.

---
+++ app/src/main/java/gr/tsiriath_android/currencyconvertor/CurrenciesJsonParser.java
@@ -0,0 +1,47 @@
+package gr.tsiriath_android.currencyconvertor;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrenciesJsonParser {
+
+    public static List<String> getCurrenciesFromJson(String jsonString) {
+
+        List<String> results = new ArrayList<>();
+        String resline;
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            String curBase = jsonObject.getString("base");
+            String curDate = jsonObject.getString("date");
+
+            JSONObject currenciesList = jsonObject.getJSONObject("rates");
+            Log.i("JSONParser- count",  Integer.toString(currenciesList.length()));
+            resline = currenciesList.toString().replace("\"","");   //convert to string and remove char "
+            resline = resline.replace(":"," - ");   //replace ":" with " - "
+            resline = resline.replace("{","");      //remove char {
+            resline = resline.replace("}","");      //remove char }
+            String[] CurArray = resline.split(","); // split string to array
+            for (int i=0; i <currenciesList.length();i++ ) {
+                //Log.i("CurrenciesJSONParser", CurArray[i]);
+                results.add(CurArray[i]);
+            }
+            resline = "EUR - 1.000";
+            results.add(resline);
+            resline = "Base currency is in " + curBase + ". Last update: " + curDate;
+            results.add(resline);
+            Log.i("JSONParser - Last line", resline);
+        }catch(JSONException e){
+            Log.e("CurrenciesJsonParser",e.getMessage());
+        }
+        return results;
+    }
+
+}
