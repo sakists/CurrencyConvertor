38cb52c190f01016cc45d554571d23713ba006fb
sakists <sakis_ts@yahoo.com>
2017-11-26 01:12:38 +0200

    2017-11-126
     Make new method on Check Network and fetch Data if auto Update is enabled. work OK.
    
    NEXT TASKS:
    1) On close setting activity re-download data from ECB
     2) On Do in Background BuiltiURI with parametrical select base currency.

--- app/src/main/java/gr/tsiriath_android/currencyconvertor/SettingsActivity.java
+++ app/src/main/java/gr/tsiriath_android/currencyconvertor/SettingsActivity.java
@@ -1,14 +1,18 @@
 package gr.tsiriath_android.currencyconvertor;
 
+import android.content.SharedPreferences;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends PreferenceActivity
     implements Preference.OnPreferenceChangeListener{
+
+    private SharedPreferences mySharedPreferences;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,14 @@ public class SettingsActivity extends PreferenceActivity
             int prefIndex = listPreference.findIndexOfValue(stringValue);
             if (prefIndex >= 0) {
                 preference.setSummary(listPreference.getEntries()[prefIndex]);
+//                mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+//                boolean BCupdFlag = mySharedPreferences.getBoolean(getString(R.string.pref_BaseCurAutoUpdate_key),Boolean.valueOf(getString(R.string.pref_BaseCurAutoUpdate_key)));
+//                if (BCupdFlag){
+//                    MainActivity.showToast(getApplicationContext(), getString(R.string.Try_to_update), Toast.LENGTH_LONG);
+//                    //String pref_baseCur = mySharedPreferences.getString(getString(R.string.pref_selectBase_key),getString(R.string.pref_selectBase_def));
+//                    FetchCurrenciesTask task = new FetchCurrenciesTask();
+//                    task.execute(this);
+//                }
             }
         } else {
             // For other preferences, set the summary to the value's simple string representation.
